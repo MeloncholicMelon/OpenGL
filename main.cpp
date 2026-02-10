93a180f714c8e5c3d221ec7dde4bc09fd4fd55bf
Chris Lee <leechris0915@gmail.com>
2026-02-10 00:20:57 -0700

    almost completed basic vertex and fragment shader implementation

--- main.cpp
+++ main.cpp
@@ -2,6 +2,28 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+
+const char* vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char* fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"} \n";
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+
 int main()
 {
 	// Initialize GLFW
@@ -41,18 +63,107 @@ int main()
 
 
 
-	// Specify the color of the background
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-	// Clean the back buffer and assign the new color to it
-	glClear(GL_COLOR_BUFFER_BIT);
-	// Swap the back buffer with the front buffer
-	glfwSwapBuffers(window);
+	float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+	};
+
+	// Create Vertex Buffer Object
+	// Stores the vertices in the GPU memory more efficient than sending it one at a time
+	// glGenBuffers generates the buffer and stores the reference ID in the variable passed
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
+	// Bind the VBO to the GL_ARRAY_BUFFER target
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); 
+
+	// binds the vertices to the currently bound GL_ARRAY_BUFFER (which is our VBO)
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// vertex shader
+
+	// create shader object and get its referenceid from OpenGL, we pass in the type of shader we want to create which is GL_VERTEX_SHADER
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	// The glShaderSource function takes the shader object to compile to as its first argument. The second argument specifies how many strings we're passing as source code
+	// //which is only one. The third parameter is the actual source code of the vertex shader and we can leave the 4th parameter to NULL.
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+		
+	// ================================================
+	// Fragment shader
+	// calculates the color of each fragment (pixel) that will be drawn on the screen
+
+	// similar to vertex shader, we create a shader object for the fragment shader, we pass in the type of shader we want to create which is GL_FRAGMENT_SHADER
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	// to check succcessful compilation after glCompileShader
+	// define an int variable to check for success and a char array to store the info log in case of failure
+	int  success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	//Then we check if compilation was successful with glGetShaderiv.If compilation failed, we should retrieve the error message with glGetShaderInfoLog and print the error message.
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// check after compiling the fragment shader as well
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	// if no errors were found the vertex shader and fragment shader was compiled successfully 
+
+	// now we need to link the shaders together to create a shader program that we can use for rendering
+	// ===============================================
+	// Shader program
+	// shader program is the final linked version of the vertex and fragment shader that we can use for rendering
+
+	// glCreateProgram creates a shader program and returns its reference ID which we store in the variable shaderProgram
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	// now we attach the previously compiled shaders to the program object then link using glLinkProgram
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	// check for linking errors (similar to shader compilation)
+	glGetProramriv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	// the result is a program object that we can activate
+
+
 
 
 
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
+		processInput(window);
+
+		// whenever we call glClear() the color buffer gets filled with the color specified in glClearColor()
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+
 		// Take care of all GLFW events
 		glfwPollEvents();
 	}
